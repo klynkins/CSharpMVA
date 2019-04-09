3a656b9e502d0af13a77eadf4c6e305241621241
Katelynn Campbell-Bishop <klyncamp23@gmail.com>
2019-04-09 15:56:28 -0400

    5-9

---
+++ OperatorsExpressionsStatement/OperatorsExpressionsStatement/Program.cs
@@ -0,0 +1,85 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OperatorsExpressionsStatement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //variable declaration
+            int x, y, a, b;
+
+            // assignment operator
+            x = 3;
+            y = 2;
+            a = 1;
+            b = 0;
+
+            // maths operators
+            // addition 
+            +
+            
+            //subtraction
+            -
+
+            // multiplication
+            *
+
+            // division
+            /
+
+            // evaluate values operator
+            // equality
+            if (x == y)
+            {
+
+            }
+
+            // greater than
+            if (x > y)
+            {
+
+            }
+
+            // less than
+            if (x < y)
+            {
+
+            }
+
+            // greater than or equal
+            if (x >= y)
+            {
+
+            }
+
+            // less than or equal
+            if (x <= y)
+            {
+
+            }
+
+            // conditional AND operator
+            if ((x > y) && (a > b))
+            {
+
+            }
+
+            // conditional OR operator
+            if ((x > y) || (a > b))
+            {
+
+            }
+
+            // in-line conditional operator
+            string message = (x == 1) ? "Car" : "Boat";
+
+            // member access and Method invocation
+            Console.WriteLine("Hi");
+        }
+    }
+}
